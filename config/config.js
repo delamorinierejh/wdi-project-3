500bca2fd7b8356c3b380b02dfb70cb7055dc9d9
delamorinierejh <delamorinierejh@gmail.com>
2016-11-02 16:30:57 +0000

    uploading to heroku

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   db: {
     test : "mongodb://localhost/swish-list-test",
     development : "mongodb://localhost/swish-list-development",
-    production : "mongodb://localhost/swish-list"
+    production : process.env.MONGODB_URI || "mongodb://heroku_xp9hccgt:6kl2cla9r6tk3vuqsivrq1kdhn@ds141937.mlab.com:41937/heroku_xp9hccgt"
   },
   secret: process.env.SECRET || "gosh this is so secret... shhh..."
 };
